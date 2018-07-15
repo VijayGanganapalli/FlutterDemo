e4e0f49572dd8334fe023a10a4603b529561d443
Vijay <vijayganganapalli@gmail.com>
2018-07-15 21:42:19 +0530

    test commit

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -6,45 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SnackBar Demo',
-      theme: ThemeData(
-        primaryColor: Colors.amber[500],
-        accentColor: Colors.amberAccent[700],  
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'SnackBar Demo',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: SnackBarPage(),
-      ),
-    );
-  }
-}
-
-class SnackBarPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Show SnackBar'),
-        onPressed: () {
-          final snackBar = SnackBar(
-            content: Text('Yay! A SnackBar!'),
-            action: SnackBarAction(
-              label: 'Undo',
-              onPressed: () {
-                // Some code to undo the change!
-              },
-            ),
-          );
-
-          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
-          Scaffold.of(context).showSnackBar(snackBar);
-        },
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(),
       ),
     );
   }
